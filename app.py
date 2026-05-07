2748a7a5aebb60556ae64ca63c5f87ef6fb51bbc
Abdul Majid <majidkhanma7777@gmail.com>
2026-05-07 16:53:09 +0500

    Files added

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from chatbot import load_faqs, build_vectorizer, get_best_match
+import os
 
 app = Flask(__name__)
 
@@ -32,4 +33,5 @@ def ask():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
